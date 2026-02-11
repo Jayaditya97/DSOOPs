937858e3fc26382ae89937066160adc76472c9d9
jayaditya97 <jayadityanagpal@gmail.com>
2026-02-11 11:30:47 +0530

    code

--- src/Binary/oddEven.java
+++
@@ -1,8 +0,0 @@
-package Binary;
-
-public class oddEven {
-    static boolean oddEven(int n){
-        if(n&& 1) == 0 return true;
-
-    }
-}
---
+++ src/Exception_Handling/basic_try_catch.java
@@ -0,0 +1,18 @@
+package Exception_Handling;
+
+import java.util.Scanner;
+
+public class basic_try_catch {
+    public static void main(String[] args) {
+        try{
+            Scanner sc = new Scanner(System.in);
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            int result = a / b;
+            System.out.println("Result = "+ result);
+        }
+        catch(Exception e){
+            System.out.println("Wrong value entered. Enter Again");
+        }
+    }
+}
